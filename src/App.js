36a806e1e5df570fdb71ff480022ce8b39a12d31
Harshdeep Jaiswal <harshdeepjaiswal7919@gmail.com>
2023-12-03 17:10:09 +0530

    main

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./index.css";
+import Main from "./containers/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <h1>User Management Dashboard</h1>
+      </center>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
